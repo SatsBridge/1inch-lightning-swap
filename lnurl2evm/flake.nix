71cc8c8cc12436f5a187945d76f35aad639e55ff
Sergey Sherkunov <me@sanerdsher.xyz>
2025-07-30 21:26:30 +0300

    Initial impl

---
+++ lnurl2evm/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "lnurl2evm";
+  inputs = {
+    flake-compat.url = "github:edolstra/flake-compat/v1.1.0";
+    deployment.url = "git+ssh://git@github.com/SatsBridge/deployment?ref=dev";
+  };
+  outputs = inputs @ { self, deployment, ... }:
+  let
+    inherit (deployment) inputs;
+    inherit (inputs)
+      nixpkgs
+      flake-utils;
+  in flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.default = pkgs.mkShell {
+      packages = builtins.attrValues {
+        inherit (pkgs) nodejs_23;
+        inherit (pkgs.importNpmLock.hooks) linkNodeModulesHook;
+      };
+      npmDeps = pkgs.importNpmLock.buildNodeModules {
+        npmRoot = ./.;
+        nodejs = pkgs.nodejs_23;
+      };
+    };
+  });
+}
---
+++ lnurl2evm/webpack.config.js
@@ -0,0 +1,32 @@
+import { fileURLToPath } from "url";
+import * as path from "path";
+
+import { TransformAsyncModulesPlugin } from "transform-async-modules-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  context: path.resolve(__dirname, "src"),
+  entry: "./main.ts",
+  experiments: {
+    outputModule: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  output: {
+    module: true,
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new TransformAsyncModulesPlugin()],
+  target: "node20.19",
+};
